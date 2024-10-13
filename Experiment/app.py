4ac6c3a1efab81c833eb51b9ab6a66e4dc1042cc
Siddarth <pullakhandam.siddartha@gmail.com>
2024-10-13 16:22:32 -0400

    commiot

--- Experiment/app.py
+++ Experiment/app.py
@@ -1,18 +1,22 @@
+import sys
+import os
+sys.path.append('/Users/siddartha/Desktop/github/Needle-In-Haystack/code') 
+
 import json
-from code.extractor import extract_multi_needle
-from code.extractor.schema import TechCompany
-from code.extractor.utils import json_to_csv
+from extractor import extract_multi_needle
+from extractor_1.schema import Football
+from extractor.utils import json_to_csv
 
 def main():
     example_needles = [
-        "Ryoshi, based in Neo Tokyo, Japan, is a private quantum computing firm founded in 2031, currently valued at $8.7 billion with 1,200 employees focused on quantum cryptography."
+        "Ajax, based in Amsterdam, Netherlands, was established in 1900 and valued at $1.2 billion, with Johan Cruyff as their greatest player."
     ]
 
     # Load the haystack text from a JSON file
-    with open('/Users/siddartha/Desktop/github/Needle-In-Haystack/haystack.txt', 'r') as f:
+    with open('/Users/siddartha/Desktop/github/Needle-In-Haystack/Experiment/haystack_exp.txt', 'r') as f:
         haystack_text = f.read()
     # Run the extraction
-    extracted_data = extract_multi_needle(schema=TechCompany, haystack=haystack_text, example_needles=example_needles)
+    extracted_data = extract_multi_needle(schema=Football, haystack=haystack_text, example_needles=example_needles)
 
     # Serialize the extracted data to a JSON file
     with open('extracted_needles.json', 'w') as f:
